e496624e9f0531eec3e5ebef2cd2d8677856825b
homeiot1 <calculushomeiot@gmail.com>
2022-09-18 11:03:19 -0700

    progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,6 +52,15 @@
     el.addEventListener('scroll', listener)
   }
 
+  window.onscroll = function () { myFunction() };
+
+  function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+  }
+
   /**
    * Navbar links active state on scroll
    */
