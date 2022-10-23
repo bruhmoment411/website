bdd690d99a6b6b28c6ab2fa2ca94fb24eeccd291
homeiot1 <calculushomeiot@gmail.com>
2022-10-23 00:17:25 -0700

    animate

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,11 +74,13 @@
   window.addEventListener('scroll', function () {
     var scrollTop = window.pageYOffset || window.scrollTop;
     var scrollPercent = scrollTop / scrollArea || 0;
-    hero2.style.top = (scrollPercent * window.innerHeight)/30 + 'px';
-    heroBlur.style.opacity = Math.min(1, scrollPercent/12);
+    if (hero2 != null && heroBlur != null) {
+      hero2.style.top = (scrollPercent * window.innerHeight) / 30 + 'px';
+      heroBlur.style.opacity = Math.min(1, scrollPercent / 12);
+    }
   });
 
-  
+
 
   /**
    * Navbar links active state on scroll
@@ -87,6 +89,7 @@
   const navbarlinksActive = () => {
     let position = window.scrollY + 200
     navbarlinks.forEach(navbarlink => {
+
       if (!navbarlink.hash) return
       let section = select(navbarlink.hash)
       if (!section) return
@@ -138,19 +141,24 @@
   /**
    * Mobile nav toggle
    */
+
   on('click', '.mobile-nav-toggle', function (e) {
     select('body').classList.toggle('mobile-nav-active')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
+
   })
 
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
+
+  let slider = document.getElementById('slider');
+
   on('click', '.scrollto', function (e) {
+    
     if (select(this.hash)) {
       e.preventDefault()
-
       let body = select('body')
       if (body.classList.contains('mobile-nav-active')) {
         body.classList.remove('mobile-nav-active')
@@ -177,9 +185,16 @@
    * Preloader
    */
   let preloader = select('#preloader');
+  let opening = document.getElementById('opening');
+  let openingtwo = document.getElementById('openingtwo');
   if (preloader) {
     window.addEventListener('load', () => {
       preloader.remove()
+      opening.style.animation = "animate-opening 0.5s cubic-bezier(.73,-0.03,.27,1.06)"
+      opening.style.animationFillMode = "forwards"
+      openingtwo.style.animation = "animate-openingtwo 0.5s cubic-bezier(.73,-0.03,.27,1.06)"
+      openingtwo.style.animationFillMode = "forwards"
+      slider.style.animation = "sliding 0.5s cubic-bezier(.73,-0.03,.27,1.06)"
     });
   }
 
